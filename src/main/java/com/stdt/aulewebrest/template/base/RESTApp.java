46cc560de42ced7aeb84804cbc3a3b6c35563baf
Timat-P <mttpecc@gmail.com>
2023-06-27 12:54:09 +0200

    Aggiunta AttrezzatureRes

--- src/main/java/com/stdt/aulewebrest/template/base/RESTApp.java
+++ src/main/java/com/stdt/aulewebrest/template/base/RESTApp.java
@@ -12,6 +12,7 @@ import com.stdt.aulewebrest.framework.security.AuthenticationRes;
 import com.stdt.aulewebrest.template.exceptions.AppExceptionMapper;
 import com.stdt.aulewebrest.framework.security.CORSFilter;
 import com.stdt.aulewebrest.template.exceptions.JacksonExceptionMapper;
+import com.stdt.aulewebrest.template.resources.AttrezzatureRes;
 import com.stdt.aulewebrest.template.resources.SimpleRes;
 
 /**
@@ -29,6 +30,7 @@ public class RESTApp extends Application {
         //con l'annotazione Path) che vogliamo pubblicare
         c.add(SimpleRes.class);
         c.add(AuthenticationRes.class);
+        c.add(AttrezzatureRes.class);
 
         //aggiungiamo il provider Jackson per poter
         //usare i suoi servizi di serializzazione e 
---
+++ src/main/java/com/stdt/aulewebrest/template/resources/AttrezzatureRes.java
@@ -0,0 +1,70 @@
+package com.stdt.aulewebrest.template.resources;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.util.ArrayList;
+import java.util.List;
+import com.stdt.aulewebrest.template.exceptions.RESTWebApplicationException;
+import com.stdt.aulewebrest.template.model.Attrezzatura;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.SecurityContext;
+
+@Path("attrezzature")
+public class AttrezzatureRes {
+    
+    @GET
+    @Path("{idaula: [0-9]+}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getPOG(
+            @PathParam("idaula") int idaula,
+            @Context UriInfo uriinfo,
+            //iniettiamo elementi di contesto utili per la verifica d'accesso
+            @Context SecurityContext sec,
+            @Context ContainerRequestContext req)
+            throws RESTWebApplicationException, SQLException, ClassNotFoundException {
+        
+        List<String> l = new ArrayList();
+        
+        InitialContext ctx;
+        try {
+            ctx = new InitialContext();
+            DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/progettoDB");
+            Connection conn = ds.getConnection();
+            
+            PreparedStatement ps = conn.prepareStatement("Select Attrezzatura.nome as Attrezzature from Fornito join Attrezzatura on Attrezzatura.ID = attrezzaturaID where aulaID = ?");
+            ps.setInt(1, idaula);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                
+                l.add(rs.getString("attrezzature"));
+                
+            }
+            
+        } catch (NamingException ex) {
+            Logger.getLogger(AttrezzatureRes.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return Response.ok(l).build();
+    }
+    
+}
