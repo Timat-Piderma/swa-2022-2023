d423466d631277088e631b9808c0d568d8dd2606
Timat-P <mttpecc@gmail.com>
2023-07-02 12:31:18 +0200

    Modificato metodo di login

--- src/main/java/com/stdt/aulewebrest/framework/security/AuthHelpers.java
+++ src/main/java/com/stdt/aulewebrest/framework/security/AuthHelpers.java
@@ -1,12 +1,24 @@
 package com.stdt.aulewebrest.framework.security;
 
+import com.stdt.aulewebrest.template.model.Amministratore;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import javax.ws.rs.core.UriInfo;
 
 /**
  *
- * Una classe di utilità di supporto all'autenticazione
- * qui tutto è finto, non usiamo JWT o altre tecnologie
+ * Una classe di utilità di supporto all'autenticazione qui tutto è finto, non
+ * usiamo JWT o altre tecnologie
  *
  */
 public class AuthHelpers {
@@ -18,10 +30,36 @@ public class AuthHelpers {
     }
 
     public boolean authenticateUser(String username, String password) {
-        return true;
+
+        InitialContext ctx;
+        try {
+            ctx = new InitialContext();
+            DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/progettoDB");
+            Connection conn = ds.getConnection();
+
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM amministratore WHERE username=?");
+
+            ps.setString(1, username);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                Amministratore a = new Amministratore();
+                a.setUsername(rs.getString("username"));
+                a.setPassword(rs.getString("password"));
+
+                if (SecurityHelpers.checkPasswordHashPBKDF2(password, a.getPassword())) {
+                    return true;
+                }
+            }
+
+        } catch (NamingException | SQLException | NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            Logger.getLogger(AuthHelpers.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
     }
 
-    public String issueToken(UriInfo context, String username) {        
+    public String issueToken(UriInfo context, String username) {
         String token = username + UUID.randomUUID().toString();
         return token;
     }
