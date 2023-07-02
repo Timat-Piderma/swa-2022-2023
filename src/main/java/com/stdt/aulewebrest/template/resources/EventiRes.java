9c92c4fbd0e856d0e1a6b06f97f6e57ff8192679
Timat-P <mttpecc@gmail.com>
2023-07-02 11:27:27 +0200

    Aggiunta API per scaricare il file iCalendar

--- src/main/java/com/stdt/aulewebrest/template/resources/EventiRes.java
+++ src/main/java/com/stdt/aulewebrest/template/resources/EventiRes.java
@@ -1,8 +1,11 @@
 package com.stdt.aulewebrest.template.resources;
 
+import com.stdt.aulewebrest.framework.security.iCal4j_Util;
 import com.stdt.aulewebrest.template.exceptions.RESTWebApplicationException;
 import com.stdt.aulewebrest.template.model.Evento;
 import com.stdt.aulewebrest.template.model.Tipologia;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URI;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -24,14 +27,17 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.StreamingOutput;
 import javax.ws.rs.core.UriInfo;
 
 @Path("eventi")
@@ -40,7 +46,7 @@ public class EventiRes {
     @GET
     @Path("{idevento: [0-9]+}")
     @Produces(MediaType.APPLICATION_JSON)
-    public EventoRes getInfoEvento(
+    public EventoRes getItem(
             @PathParam("idevento") int idevento,
             @Context UriInfo uriinfo,
             //iniettiamo elementi di contesto utili per la verifica d'accesso
@@ -228,20 +234,21 @@ public class EventiRes {
 
     }
 
-    @POST
-    @Path("{idevento: [0-9]+}")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @PUT
+    @Consumes("application/json")
     public Response UpdateItem(
             @Context ContainerRequestContext req,
             @Context UriInfo uriinfo,
-            @FormParam("data") Date data,
-            @FormParam("oraInizio") Time oraInizio,
-            @FormParam("oraFine") Time oraFine,
+            @FormParam("idevento") String idevento,
+            @FormParam("giorno") String giorno,
+            @FormParam("oraInizio") String oraInizio,
+            @FormParam("oraFine") String oraFine,
             @FormParam("descrizione") String descrizione,
             @FormParam("nome") String nome,
-            @FormParam("tipo") String tipo,
+            @FormParam("tipologia") String tipologia,
             @FormParam("idaula") String idaula,
-            @FormParam("idresponsabile") String idresponsabile
+            @FormParam("idresponsabile") String idresponsabile,
+            @FormParam("idcorso") String idcorso
     ) throws SQLException, NamingException {
 
         InitialContext ctx;
@@ -249,15 +256,28 @@ public class EventiRes {
         DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/progettoDB");
         Connection conn = ds.getConnection();
 
-        PreparedStatement ps = conn.prepareStatement("Select * from Evento where ID = ?");
-        ps.setDate(1, (java.sql.Date) data);
-        ps.setTime(2, oraInizio);
-        ps.setTime(3, oraFine);
+        PreparedStatement ps = conn.prepareStatement("UPDATE evento SET giorno=?,oraInizio=?,oraFine=?,descrizione=?, nome=?, tipologia=?, aulaID=?, responsabileID=?, corsoID=?, version=? WHERE ID=? and version=?");
+        ps.setString(1, giorno);
+        ps.setString(2, oraInizio);
+        ps.setString(3, oraFine);
         ps.setString(4, descrizione);
         ps.setString(5, nome);
-        ps.setString(6, tipo);
+        ps.setString(6, tipologia);
         ps.setString(7, idaula);
         ps.setString(8, idresponsabile);
+        ps.setString(9, idcorso);
+
+        PreparedStatement psversion = conn.prepareStatement("select version form evento where ID=?");
+        psversion.setString(1, idevento);
+        ResultSet rsversion = psversion.executeQuery();
+        rsversion.next();
+
+        long current_version = rsversion.getInt("version");
+        long next_version = current_version + 1;
+
+        ps.setLong(10, next_version);
+        ps.setString(11, idevento);
+        ps.setLong(12, current_version);
 
         if (ps.executeUpdate() == 1) {
 
@@ -268,11 +288,67 @@ public class EventiRes {
                     .path(EventoRes.class)
                     .path(EventoRes.class, "getItem")
                     .build(keys.getInt(1));
-            return Response.created(uri).build();
+            return Response.noContent().build();
         } else {
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
 
     }
 
+    //esempio di download binario in streaming
+    @Path("{idaula: [0-9]+}/{giorno: [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]}/attachment")
+    @GET
+    @Produces("application/ics")
+    public Response getAttachment(
+            @PathParam("idaula") int idaula,
+            @PathParam("giorno") String giorno) {
+
+        List<Evento> result = new ArrayList<>();
+
+        InitialContext ctx;
+        try {
+            ctx = new InitialContext();
+            DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/progettoDB");
+            Connection conn = ds.getConnection();
+
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM evento WHERE WEEK(evento.giorno)=WEEK(?) AND aulaID=?");
+
+            ps.setString(1, giorno);
+            ps.setInt(2, idaula);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Evento evento = new Evento();
+
+                evento.setData(LocalDate.parse(rs.getString("giorno")));
+                evento.setOraInizio(LocalTime.parse(rs.getString("oraInizio")));
+                evento.setOraFine(LocalTime.parse(rs.getString("oraFine")));
+                evento.setNome(rs.getString("nome"));
+                evento.setDescrizione(rs.getString("descrizione"));
+                evento.setTipologia(Tipologia.valueOf(rs.getString("tipologia")));
+
+                result.add(evento);
+
+            }
+
+            String s = iCal4j_Util.CalendarUtil(result);
+
+            final byte[] attachment = s.getBytes();
+
+            StreamingOutput out = new StreamingOutput() {
+                @Override
+                public void write(OutputStream output) throws IOException, WebApplicationException {
+                    output.write(attachment);
+                }
+            };
+            return Response
+                    .ok(out)
+                    .header("content-disposition", "attachment; filename=eventi.ics")
+                    .build();
+        } catch (NamingException | SQLException | IOException ex) {
+            Logger.getLogger(EventiRes.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return Response.status(Response.Status.NOT_ACCEPTABLE).build();
+    }
 }
